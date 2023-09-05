9032167f99b9e48c637adb363c60974a453e279b
Alekseev53 <alekseev.alex.romanovich@gmail.com>
2023-09-05 14:50:29 +0300

    теперь работает на кратных корнях не четного порядка

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def find_all_roots(f, range_l, range_r, eps=1e-5, max_roots=100, max_attempts=10
         l, r = min(l, r), max(l, r)
 
         root = dih(f, l, r, eps)
-        if root is not None and abs(f(root)) < eps:
+        while root is not None and abs(f(root+eps/10)) < eps:
             roots.append(root)
             f = lambda x, f=f, root=root: f(x) / (x - root) if x != root else 0
 
@@ -32,7 +32,7 @@ def find_all_roots(f, range_l, range_r, eps=1e-5, max_roots=100, max_attempts=10
 
 
 def f(x):
-    return (x - 2) * ((x - 3) ** 2) * (x - 7)
+    return (x - 2) * ((x - 3) ** 3) * (x - 7)
 
 roots = find_all_roots(f, -10, 10)
 print([round(x,2) for x in roots])
